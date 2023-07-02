986162425bbd9e474b0d3dc3a9afd685d57e53c2
Leonardo Maron <leo22.maron@hotmail.com>
2023-07-01 22:06:37 -0300

    Add Import/Export functions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
+import { useFormContext } from 'react-hook-form';
+
 import { formSchema } from '../utils/form';
+import type { FormValues } from '../utils/form';
 
 export function Header() {
+  const { handleSubmit, reset } = useFormContext<FormValues>();
+
   function handleImportFile(e: React.ChangeEvent<HTMLInputElement>) {
     if (!e.target.files?.[0]) return;
 
@@ -10,17 +15,30 @@ export function Header() {
       const result = reader.result as string;
 
       try {
-        const fileAsJson = JSON.parse(result);
+        const parsedFile = JSON.parse(result);
 
-        if (!formSchema.isValidSync(fileAsJson)) throw new Error();
+        if (!formSchema.isValidSync(parsedFile)) throw new Error();
 
-        console.log(fileAsJson);
+        reset(parsedFile);
       } catch (e) {
         alert('Invalid file.');
       }
     };
 
     reader.readAsText(e.target.files[0]);
+  }
+
+  function handleExportFile(data: FormValues) {
+    const file = new Blob([JSON.stringify(data)], {
+      type: 'text/plain;charset=utf-8',
+    });
+    const element = document.createElement('a');
+
+    element.href = URL.createObjectURL(file);
+    element.download = `${data.name}.json`;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
   }
 
   return (
@@ -30,10 +48,18 @@ export function Header() {
       <div className="flex gap-2">
         <label className="cursor-pointer rounded-lg bg-violet-600 px-4 py-2 font-medium text-white transition hover:bg-violet-700">
           Import
-          <input type="file" onChange={handleImportFile} className="hidden" />
+          <input
+            type="file"
+            accept=".json"
+            onChange={handleImportFile}
+            className="hidden"
+          />
         </label>
 
-        <button className="rounded-lg bg-violet-600 px-4 py-2 font-medium text-white transition hover:bg-violet-700">
+        <button
+          onClick={handleSubmit(handleExportFile)}
+          className="rounded-lg bg-violet-600 px-4 py-2 font-medium text-white transition hover:bg-violet-700"
+        >
           Export
         </button>
       </div>
