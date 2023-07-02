4df1f8889d791070d704d7c152a228b9a2a55f2b
Leonardo Maron <leo22.maron@hotmail.com>
2023-07-01 21:31:34 -0300

    Add title editing

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,20 @@
+import { Controller } from 'react-hook-form';
+
 import { Image } from './Image';
 
 export function Editor() {
   return (
     <main className="flex flex-1 flex-col gap-2 rounded-lg bg-neutral-800 p-2">
-      <h2 className="text-center text-2xl font-bold text-white">My Work</h2>
+      <Controller
+        name="name"
+        render={({ field }) => (
+          <input
+            type="text"
+            className="w-full rounded-lg bg-transparent text-center text-xl font-bold text-white"
+            {...field}
+          />
+        )}
+      />
 
       <Image />
     </main>
